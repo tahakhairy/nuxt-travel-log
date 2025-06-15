d65053cf161a10c4d20e5ef3feaf75d4bbc26f60
tahakhairy <tahakhairy@ymail.com>
2025-06-16 02:15:38 +0300

    :sparkles: login with github

---
+++ lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { createAuthClient } from "better-auth/vue";
+
+export const authClient = createAuthClient({
+  // you can pass client configuration here
+
+});
--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,7 @@
         <p class="py-6">
           Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys
         </p>
-        <button class="btn btn-accent">
-          Sign In with Github <Icon name="tabler:brand-github" size="20" />
-        </button>
+        <AuthButton />
       </div>
     </div>
   </div>
