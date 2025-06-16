373c7262403dbcc0ee40845a0113415ace052495
Taha Khairy <tahakhairy@ymail.com>
2025-06-16 03:22:22 +0300

    :sparkles: add user avatar and signout (#57)

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
-const { loading } = storeToRefs(useAuthStore());
-const { signIn } = useAuthStore();
+const { loading, user } = storeToRefs(useAuthStore());
+const { signIn, signOut } = useAuthStore();
 </script>
 
 <template>
+  <div v-if="!loading && user" class="dropdown drawer-end ">
+    <div
+      tabindex="0"
+      role="button"
+      class="btn m-1"
+    >
+      <div v-if="user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="user?.image" :alt="user.name">
+        </div>
+      </div>
+      {{ user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <button @click="signOut">
+          <Icon name="tabler:logout-2" size="24" /> Sign Out
+        </button>
+      </li>
+    </ul>
+  </div>
   <button
+    v-else
     :disabled="loading"
     class="btn btn-accent"
     @click="signIn"
--- stores/auth.ts
+++ stores/auth.ts
@@ -1,19 +1,29 @@
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/vue";
 
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("authStore", () => {
-  const loading = ref(false);
+  const session = authClient.useSession();
+  const user = computed(() => session.value.data?.user);
+  const loading = computed(() => session.value.isPending || session.value.isRefetching);
 
   async function signIn() {
-    loading.value = true;
     await authClient.signIn.social({
       provider: "github",
       callbackURL: "/dashboard",
       errorCallbackURL: "/error",
     });
-    loading.value = false;
   }
 
-  return { signIn, loading };
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess() {
+          navigateTo("/");
+        },
+      },
+    });
+  }
+
+  return { signIn, signOut, loading, user };
 });
