00667708976b5afe815f80371e4fa336b5c2dbcd
Taha Khairy <tahakhairy@ymail.com>
2025-06-21 06:14:27 +0300

    :sparkles: replace daisyui with nuxtui (#58)

--- components/app/navbar.vue
+++ components/app/navbar.vue
@@ -3,17 +3,15 @@
 </script>
 
 <template>
-  <div class="navbar bg-primary text-primary-content px-6">
-    <div class="navbar-start">
-      <NuxtLink to="/" class=" text-xl font-bold mx-3">
+  <nav class="text-primary-content py-4 border-b border-default">
+    <UContainer class="flex justify-between items-center !px-0">
+      <NuxtLink to="/" class=" text-xl font-bold ">
         <h1>Travel Log</h1>
       </NuxtLink>
-    </div>
-
-    <div class="navbar-end items-center flex gap-4">
-      <AppThemeToggle />
-
-      <AuthButton />
-    </div>
-  </div>
+      <div class="navbar-end items-center flex gap-4">
+        <AppThemeToggle />
+        <AuthButton />
+      </div>
+    </UContainer>
+  </nav>
 </template>
--- components/auth-button.vue
+++ components/auth-button.vue
@@ -1,44 +1,63 @@
 <script setup lang="ts">
+import { UButton } from "#components";
+
 const { loading, user } = storeToRefs(useAuthStore());
 const { signIn, signOut } = useAuthStore();
 </script>
 
 <template>
-  <div v-if="!loading && user" class="dropdown drawer-end ">
-    <div
-      tabindex="0"
-      role="button"
-      class="btn m-1"
+  <UDropdownMenu
+    v-if="!loading && user"
+    size="lg"
+    :items="[{
+      label: 'Sign Out',
+      slot: 'icon' as const,
+      onSelect() {
+        signOut()
+      },
+    }]"
+  >
+    <UButton
+      :label="`${user.name}`"
+      color="neutral"
+      variant="outline"
+      icon="i-lucide-menu"
     >
-      <div v-if="user.image" class="avatar">
-        <div class="w-8 rounded-full">
-          <img :src="user?.image" :alt="user.name">
-        </div>
-      </div>
-      {{ user.name }}
-    </div>
-    <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
-      <li>
-        <button @click="signOut">
-          <Icon name="tabler:logout-2" size="24" /> Sign Out
-        </button>
-      </li>
-    </ul>
-  </div>
-  <button
+      <template #leading>
+        <UAvatar
+          v-if="user.image"
+          size="sm"
+          :src="user.image"
+        />
+      </template>
+    </UButton>
+
+    <template #icon-leading>
+      <Icon
+        name="tabler:logout-2"
+        size="24"
+        class="text-error-500"
+      />
+    </template>
+  </UDropdownMenu>
+  <UButton
     v-else
     :disabled="loading"
-    class="btn btn-accent"
     @click="signIn"
   >
     Sign In with Github
-    <span v-if="loading" class="loading loading-spinner loading-lg" />
+    <Icon
+      v-if="loading"
+      name="tabler:loader-2"
+      class="animate-spin"
+      size="24"
+    />
     <Icon
       v-else
       name="tabler:brand-github"
-      size="20"
+      size="24"
     />
-  </button>
+  </UButton>
 </template>
 
 <style scoped>
