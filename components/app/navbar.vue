260881115898479909e27a3e2036e4fa1096c5cf
Taha Khairy <tahakhairy@ymail.com>
2025-06-26 04:18:08 +0300

    :sparkles: add app sidebar (#61)

--- components/app/navbar.vue
+++ components/app/navbar.vue
@@ -4,7 +4,7 @@
 
 <template>
   <nav class="text-primary-content py-4 border-b border-default">
-    <UContainer class="flex justify-between items-center !px-0">
+    <div class="flex justify-between items-center px-8">
       <NuxtLink to="/" class=" text-xl font-bold ">
         <h1>Travel Log</h1>
       </NuxtLink>
@@ -14,6 +14,6 @@
         </ClientOnly>
         <AuthButton />
       </div>
-    </UContainer>
+    </div>
   </nav>
 </template>
